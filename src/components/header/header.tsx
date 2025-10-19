9c2f1ebdc9265d49344b6c44ed1ae0434f81fc6e
Jason Dove <jason.r.dove@gmail.com>
2025-10-19 13:33:43 -0400

    feat: minor UI updates

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Header: React.FunctionComponent<T> = () => {
     return (
         <header className="md:w-[50%] md:sticky md:top-0 md:max-h-screen md:flex md:flex-col md:py-24">
-            <div>Jason Dove</div>
-            <div>Web Developer</div>
+            <div className="text-4xl mb-3">Jason Dove</div>
+            <div className="text-lg mb-3">Web Developer</div>
             <p>pretty cool dude</p>
         </header>
     );
--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -13,10 +13,9 @@ const Resume: React.FunctionComponent<T> = () => {
                 return (
                     <div
                         key={item.company}
-                        className={`mb-2 p-2 cursor-pointer ${hoverItem !== index && typeof hoverItem === 'number' && 'opacity-25'} transition-opacity duration-400`}
+                        className={`mb-2 p-2 cursor-pointer rounded-md ${hoverItem !== index && typeof hoverItem === 'number' && 'opacity-25'} transition-opacity duration-400`}
                         onMouseEnter={() => setHoverItem(index)}
                         onMouseLeave={() => setHoverItem(null)}
-                        onClick={() => console.log(hoverItem)}
                     >
                         <div className="flex mb-2">
                             <span className="mr-auto text-base">{item.title}</span>
@@ -25,7 +24,7 @@ const Resume: React.FunctionComponent<T> = () => {
                                 <span>{item.yearStart} - {item.yearEnd}</span>
                             </span>
                         </div>
-                        <ul className="text-sm">
+                        <ul className="text-sm list-disc ms-4">
                             {item.bullets.map((bullet) => {
                                 return (
                                     <li key={bullet} className="mb-1">{bullet}</li>
